4a2b06172a548a8aeca4f46b1dccf0ac319e43d7
Judah Oluwaseeki <judah.oluwaseeki@wyzetalk.com>
2025-05-03 17:14:21 +0200

    refactor: updated group contribution styling

--- app/(protected)/group-contribution/page.tsx
+++ app/(protected)/group-contribution/page.tsx
@@ -12,10 +12,10 @@ const ProfilePage = async ({
 
   return (
     <div className="flex h-screen items-center ">
-      <main className="pb-[40px] md:rounded-3xl md:shadow-2xl bg-turquoise-50 h-full md:h-[700px] border md:border-gray-400/2 w-[400px] md:w-[350px] mx-auto overflow-scroll">
-      <Header asset={asset} />
-      <Body />
-    </main>
+      <main className="pb-[40px] md:rounded-3xl md:shadow-2xl bg-turquoise-50 h-full md:h-[700px] border md:border-gray-400/2 w-[400px] md:w-[350px] mx-auto md:overflow-hidden overflow-scroll">
+        <Header asset={asset} />
+        <Body />
+      </main>
     </div>
   );
 };
--- components/(protected)/group-contribution/Body.tsx
+++ components/(protected)/group-contribution/Body.tsx
@@ -4,14 +4,14 @@ import { UploadPaymentDashboard } from "@/components/Buttons";
 
 const Body = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative md:h-[550px] h-screen">
       <div className="px-[20px] flex flex-col gap-3">
         {groupUsersInfos.map((p) => (
           <ProfileInfoCard {...p} key={p.name} />
         ))}
       </div>
-      <div className="fixed bottom-5 w-full ">
-        <div className="w-full px-7">
+      <div className="fixed md:-bottom-8 bottom-5 left-6 w-full md:absolute">
+        <div className="md:w-[300px] w-full px-7">
           <UploadPaymentDashboard />
         </div>
       </div>
